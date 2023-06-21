1a52985b0a2a36a08cf9458f6ff05c0321a6af67
Argyn Sagash <argyn.sagash@gmail.com>
2023-06-21 11:05:11 +0600

    Add Solution Number Of Steps To Reduce A Number To Zero

--- src/main/kotlin/com/argynsagash/leetcode/beginner/NumberOfStepsToReduceANumberToZero.kt
+++ src/main/kotlin/com/argynsagash/leetcode/beginner/NumberOfStepsToReduceANumberToZero.kt
@@ -1,2 +1,27 @@
 package com.argynsagash.leetcode.beginner
 
+class NumberOfStepsToReduceANumberToZero {
+    fun numberOfSteps(num: Int): Int {
+        var step = 0
+        var number = num
+        while (number != 0) {
+            if (number % 2 == 0) {
+                number /= 2
+                step += 1
+            } else {
+                number -= 1
+                step += 1
+            }
+        }
+
+        return step
+    }
+}
+
+fun main() {
+    val solution = NumberOfStepsToReduceANumberToZero()
+    println(solution.numberOfSteps(14))
+    println(solution.numberOfSteps(8))
+    println(solution.numberOfSteps(123))
+
+}
